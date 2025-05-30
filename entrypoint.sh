8a965622a5f524d2287faec3992b29bc65a8912f
fmillerrzero <150078390+fmillerrzero@users.noreply.github.com>
2025-05-30 09:39:38 -0700

    poop

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-# Use the PORT environment variable if set, otherwise default to 10000
 PORT=${PORT:-10000}
-
-# Start Gunicorn with the resolved port and increased timeout
 exec gunicorn --bind 0.0.0.0:$PORT --timeout 120 app:app
